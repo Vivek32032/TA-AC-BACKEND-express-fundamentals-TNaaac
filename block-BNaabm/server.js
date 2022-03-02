3b1746a8840f4be1ad717c6a4339bd2cb05a9e33
Vivek Kumar <vivek32032@gmail.com>
2022-03-02 10:51:45 +0530

    refactor

--- block-BNaabm/server.js
+++ block-BNaabm/server.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var app = express();
 
+//middleware
 app.use('/',(req,res,next)=>{
 console.log(req.method,req.url);
 next();
 })
 
 app.get("/",(req,res)=>{
-res.end('Welcome  to Express');})
+res.send('Welcome  to Express');})
 
 
 app.listen(4000,()=>{
