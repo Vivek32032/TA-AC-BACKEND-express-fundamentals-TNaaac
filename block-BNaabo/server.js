5b71b5a03c302b4aa1cadb74768b27211b2c0397
Vivek Kumar <vivek32032@gmail.com>
2022-03-02 22:36:27 +0530

    add static folder

--- block-BNaabo/server.js
+++ block-BNaabo/server.js
@@ -2,10 +2,6 @@ var express = require(`express`);
 
 var app= express();
 
-app.use('/',(req,res,next)=>{
-    console.log(req.method,req.url);
-    next();
-})
 app.use(express.json());
 
 app.use(express.urlencoded({extended:false}));
@@ -13,10 +9,10 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.static(__dirname + `/public`));
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+ '/index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.post(`/`,(req,res)=>{
+app.post(`/json`,(req,res)=>{
     console.log(req.body)
 })
 
