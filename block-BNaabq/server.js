7d08d2d33e5b3a28eaf066ff938071f8e2156d84
Vivek Kumar <vivek32032@gmail.com>
2022-03-03 18:43:49 +0530

    add public folder

--- block-BNaabq/server.js
+++ block-BNaabq/server.js
@@ -1,21 +1,29 @@
 var express = require('express');
-var logger = require('parser');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 
 var app = express();
 
+app.use(express.json())
 app.use(logger('dev'));
+app.use(express.urlencoded({extended :false}))
+app.use(express.static(__dirname + "/public"));
 app.use(cookieParser());
 
+app.use((req, res, next ) => {
+    console.log(req.cookies);
+    next();
+});
 
-app.use( ( req, res, next ) => {
+app.use( "/about", (req, res, next ) => {
     res.cookie( 'username', "Vivekkr." );
     next();
 } );
 
-app.get( '/about', ( req, res ) => {
-    res.send("morgan and cookie parser");
-} )
+app.get( '/', ( req, res ) => {
+    res.sendFile(__dirname+"/index.html");
+});
+
 
 
 app.listen(4545,()=>{
