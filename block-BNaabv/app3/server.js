f5b7330a99b84199323840d0a6ef8160783eeb87
Vivek Kumar <vivek32032@gmail.com>
2022-03-04 23:57:06 +0530

    add error handles

--- block-BNaabv/app3/server.js
+++ block-BNaabv/app3/server.js
@@ -1,10 +1,23 @@
 var express = require("express");
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 
 var app = express();
+
+//middleware
 
 app.use(express.json());
 app.use(express.urlencoded( {extended : false}));
 app.use(express.static(__dirname + 'public'))
+app.use(logger('dev'));
+app.use(cookieParser());
+
+app.use((req,res,next)=>{
+    res.cookie('username',"xyz");
+    next();
+})
+
+//routes
 
 app.get('/',(req,res)=>{
     res.send('<h2 style="color:blue">Welcome to express<h2>')
@@ -12,6 +25,27 @@ app.get('/',(req,res)=>{
 
 app.get('/about',(req,res)=>{
     res.send('My name is qwerty')
+})
+
+app.post('/form',(req,res)=>{
+    res.json(req.body);
+})
+
+app.post('/json',(req,res)=>{
+    res.json(req.body);
+})
+app.get('/users/:username',(req,res)=>{
+    var username = req.params.username;
+    res.send(`<h2>${username}</h2>`)
+});
+//error handlers
+app.use((req,res,next)=>{
+    res.send('Page Not Found');
+})
+
+//custom middleware
+app.use((err,req,res,next)=>{
+    res.send(err);
 })
 
 app.listen(3000,()=>{
